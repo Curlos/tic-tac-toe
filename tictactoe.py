cc15f7a50e70fbb4a16c61d7d3479286ac3819e4
Carlos Martinez <curlosmart@gmail.com>
2021-02-18 09:11:26 -0500

    refactor code and 3rd gamemode added

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,11 @@
 from random import choice
+import time
 
 
 def player_turn(board, squares, all_pos, player_num, all_winning_pos):
     ''' Asks the player to enter a valid, empty square and adds it to the board. '''
     while True:
+        board = update_board(board, squares)
         print(board)
         player_square = input(
             f"Player {player_num}'s turn.\nEnter square: (e.g. A0): ")
@@ -26,19 +28,27 @@ def player_turn(board, squares, all_pos, player_num, all_winning_pos):
                 'Square invalid, please enter a valid, empty square from the following list: [A0, A1, A2, B0, B1, B2, C0, C1, C2]')
 
 
-def ai_turn(board, squares, all_pos, all_winning_pos):
+def bot_turn(board, squares, all_pos, player_num, all_winning_pos):
     ''' Asks the player to enter a valid, empty square and adds it to the board. '''
+    board = board = update_board(board, squares)
     print(board)
-    ai_square = choice(list(squares.keys()))
+    bot_square = choice(list(squares.keys()))
+    time.sleep(1)
 
     while True:
-        if squares[ai_square] != ' ':
-            ai_square = choice(list(squares.keys()))
+        if squares[bot_square] != ' ':
+            bot_square = choice(list(squares.keys()))
         else:
             break
-    squares[ai_square] = 'O'
-    all_pos.append(ai_square)
-    return check_all_pos(all_winning_pos, all_pos, 'O')
+
+    if player_num == '1':
+        squares[bot_square] = 'X'
+        all_pos.append(bot_square)
+        return check_all_pos(all_winning_pos, all_pos, 'X')
+    else:
+        squares[bot_square] = 'O'
+        all_pos.append(bot_square)
+        return check_all_pos(all_winning_pos, all_pos, 'O')
 
 
 def check_all_pos(all_winning_pos, all_pos, letter):
@@ -61,6 +71,75 @@ def update_board(board, squares):
     board += f"2 | {squares['A2'].center(2)} | {squares['B2'].center(2)} | {squares['C2'].center(2)} |\n"
 
     return board
+
+
+def gamemode_player_vs_player(board, squares, all_x_pos, all_o_pos, all_winning_pos):
+    player_one = player_turn(
+        board, squares, all_x_pos, '1', all_winning_pos)
+    board = update_board(board, squares)
+
+    # Returns true if the player has placed three X in positions where it can be crossed
+    if(player_one):
+        print(board)
+        print(f"Game over! Player one has won the game!")
+        return True
+
+    player_two = player_turn(
+        board, squares, all_o_pos, '2', all_winning_pos)
+    board = update_board(board, squares)
+
+    # Returns true if the player has placed three X in positions where it can be crossed
+    if(player_two):
+        print(board)
+        print(f"Game over! Player two has won the game!")
+        return True
+
+    return False
+
+
+def gamemode_player_vs_bot(board, squares, all_x_pos, all_o_pos, all_winning_pos):
+    player_one = player_turn(
+        board, squares, all_x_pos, '1', all_winning_pos)
+    board = update_board(board, squares)
+
+    # Returns true if the player has placed three X in positions where it can be crossed
+    if(player_one):
+        print(board)
+        print(f"Game over! Player one has won the game!")
+        return True
+
+    player_bot = bot_turn(
+        board, squares, all_o_pos, '2', all_winning_pos)
+    board = update_board(board, squares)
+
+    if(player_bot):
+        print(board)
+        print(f"Game over! Bot1 has won the game!")
+        return True
+
+    return False
+
+
+def gamemode_bot_vs_bot(board, squares, all_x_pos, all_o_pos, all_winning_pos):
+    player_bot1 = bot_turn(
+        board, squares, all_x_pos, '1', all_winning_pos)
+    board = update_board(board, squares)
+
+    if(player_bot1):
+        print(board)
+        print(f"Game over! Bot1 has won the game!")
+        return True
+
+    player_bot2 = bot_turn(
+        board, squares, all_o_pos, '2', all_winning_pos)
+    board = update_board(board, squares)
+
+    if(player_bot2):
+        print(board)
+        print(f"Game over! Bot2 has won the game!")
+        return True
+
+    return False
 
 
 def start():
@@ -90,40 +169,25 @@ def start():
     all_winning_pos.append(['A2', 'B2', 'C2'])
     all_winning_pos.append(['A2', 'B1', 'C0'])
 
-    game_type = input(
-            "Choose a game mode [Enter '1' or '2']:\n1. Player vs. Player\n2. Player vs. AI\n")
+    gamemode = input(
+        "Choose a game mode [Enter '1' or '2']:\n1. Player vs. Player\n2. Player vs. AI\n3. Bot vs. Bot\n")
 
     while running:
-        player_one = player_turn(
-            board, squares, all_x_pos, '1', all_winning_pos)
-        board = update_board(board, squares)
 
-        # Returns true if the player has placed three X in positions where it can be crossed
-        if(player_one):
-            print(board)
-            print(f"Game over! Player one has won the game!")
-            break
-
-        print(len(all_x_pos) + len(all_o_pos))
-
-        if(game_type == '1'):
-            player_two = player_turn(
-                board, squares, all_o_pos, '2', all_winning_pos)
-            board = update_board(board, squares)
-
-            # Returns true if the player has placed three X in positions where it can be crossed
-            if(player_two):
-                print(board)
-                print(f"Game over! Player two has won the game!")
+        if(gamemode == '1'):
+            game_over = gamemode_player_vs_player(
+                board, squares, all_x_pos, all_o_pos, all_winning_pos)
+            if(game_over):
                 break
-        elif(game_type == '2'):
-            player_ai = ai_turn(
-                board, squares, all_o_pos, all_winning_pos)
-            board = update_board(board, squares)
-
-            if(player_ai):
-                print(board)
-                print(f"Game over! Player AI has won the game!")
+        elif(gamemode == '2'):
+            game_over = gamemode_player_vs_bot(
+                board, squares, all_x_pos, all_o_pos, all_winning_pos)
+            if(game_over):
+                break
+        elif(gamemode == '3'):
+            game_over = gamemode_bot_vs_bot(
+                board, squares, all_x_pos, all_o_pos, all_winning_pos)
+            if(game_over):
                 break
         else:
             print('Invalid game mode entered. Please try again.')
